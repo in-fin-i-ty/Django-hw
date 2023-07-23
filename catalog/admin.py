c444d2a889f8da61436b67f0b23fb63308c16a43
in-fin-i-ty <128904385+in-fin-i-ty@users.noreply.github.com>
2023-07-23 23:38:41 +0300

    add settings

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,18 @@
+from .models import Product, Category
+
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    verbose_name = 'Категории'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('description',)
+    verbose_name = 'Продукты'
+
