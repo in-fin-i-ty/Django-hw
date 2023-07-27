bacf785e35ce8de784a35d4466481e7eba7594a1
in-fin-i-ty <128904385+in-fin-i-ty@users.noreply.github.com>
2023-07-27 23:27:51 +0300

    update

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import contacts, index
+from .views import contacts, index, product
+from .apps import MainConfig
+
+app_name = MainConfig.name
 
 urlpatterns = [
-    path('main.contacts/', contacts),
-    path('', index)
+    path('main.contacts/', contacts, name='contacts'),
+    path('', index, name='index'),
+    path('<int:pk>main.product/', product, name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'object_list': Product.objects.all()[:100],
+        'title': 'Продукты'
+    }
+    return render(request, 'main/index.html', context)
+
+
+def product(request, pk):
+    product_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(pk=pk),
+        'title': f'{product_item.name}'
+    }
+    return render(request, 'main/product.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты'
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f"{name} - ({phone}): {message}")
-    return render(request, 'main/contacts.html')
+    return render(request, 'main/contacts.html', context)
