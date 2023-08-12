b9952f3e910156c084530f2ed6f7722bd7180cce
in-fin-i-ty <128904385+in-fin-i-ty@users.noreply.github.com>
2023-08-12 21:37:07 +0300

    update

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import contacts, index, product
+from .views import contacts, IndexListView, ProductDetailView, BlogCreateView, BlogListView, BlogDetailView,\
+    BlogUpdateView, BlogDeleteView
 from .apps import MainConfig
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('main.contacts/', contacts, name='contacts'),
-    path('', index, name='index'),
-    # path('<int:pk>main.product/', product, name='product'),
-    path('product/<int:pk>/', product, name='product'),
+    path('catalog.contacts/', contacts, name='contacts'),
+    path('', IndexListView.as_view(), name='index'),
+    # path('<int:pk>catalog.product/', product, name='product'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='detail_product'),
+    path('create/', BlogCreateView.as_view(), name='create_blog'),
+    path('view/', BlogListView.as_view(), name='view_blog'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='detail_blog'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='update_blog'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog'),
 ]
